0463eb7252a509358b1fb94cf8743c59168f0d1f
HHalfhide <harryhalfhide@gmail.com>
2020-08-24 16:50:21 +0100

    Update ServiceLayerImpl.java

--- OrderBook/src/main/java/com/mthree/OrderBook/service/ServiceLayerImpl.java
+++ OrderBook/src/main/java/com/mthree/OrderBook/service/ServiceLayerImpl.java
@@ -17,8 +17,10 @@ import com.mthree.OrderBook.entities.OrderVersion;
 import com.mthree.OrderBook.entities.Party;
 import com.mthree.OrderBook.entities.Stock;
 import com.mthree.OrderBook.entities.Trade;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,6 +72,87 @@ public class ServiceLayerImpl implements serviceLayer{
         writeAudit("ORDER " + order.getId() + ": " + order.getParty().getSymbol() + buyString + order.getStock().getSymbol() + " CANCELLED.");
     }
     
+    public void addBuyOrder(Order order, OrderVersion buyOrderVersion){
+        
+        // create variable which determine whether to keep trading 
+        boolean trading = true;
+        
+        // Add the order to the database and orderVersion to the Database
+        orderRepository.save(order);
+        
+        
+        while(trading){
+            // Add order version to database and assign complete object fields from db to buyOrderVersion
+            buyOrderVersion = orderVersionRepository.save(buyOrderVersion);
+            
+            // Get the cheapest active sell order version
+            OrderVersion sellOrderVersion = orderVersionRepository.getActiveSellOrderVersionsForStock(order.getStock()).get(0);
+
+            // Check if the price of the sell order version is lower than or equal to the price of the buy order version
+            // If so: do trading logic
+            // If not: set trading to false
+            if (sellOrderVersion.getPrice().compareTo(buyOrderVersion.getPrice()) == -1 || sellOrderVersion.getPrice().compareTo(buyOrderVersion.getPrice()) == 0) {
+
+                // The price of the trade will be executed at the price of the sell order version
+                BigDecimal tradePrice = sellOrderVersion.getPrice();
+
+                // The size of the trade will be the minimum size of the buy and sell order versions
+                int tradeSize = Math.min(sellOrderVersion.getSize(), buyOrderVersion.getSize());
+
+                // Change the order versions to inactive and update them
+                sellOrderVersion.setIsActive(false);
+                orderVersionRepository.save(sellOrderVersion);
+                buyOrderVersion.setIsActive(false);
+                orderVersionRepository.save(buyOrderVersion);
+
+                // Create and add the trade
+                Trade trade = new Trade();
+                trade.setBuyOrderVersion(buyOrderVersion);
+                trade.setSellOrderVersion(sellOrderVersion);
+                trade.setPrice(tradePrice);
+                trade.setSize(tradeSize);
+                trade.setTime(LocalDateTime.now());
+                tradeRepository.save(trade);
+
+                // create and save new buy order version if there is remaining size, if there is no remaining size set go again to false
+                if (buyOrderVersion.getSize() > tradeSize) {
+                    OrderVersion newBuyOrderVersion = copyOrderVersion(buyOrderVersion, tradeSize);
+                    
+                    // set buy order version to the fully formed newBuyOrderVersion (with sql auto generated id) for the next performance of the while loop
+                    // and also add new buy order version
+                    buyOrderVersion = newBuyOrderVersion;
+
+                } else {
+                    trading = false;
+                }
+
+                // create and save new sell order version if there is remaining size
+                if (sellOrderVersion.getSize() >tradeSize ) {
+                    OrderVersion newSellOrderVersion = copyOrderVersion(sellOrderVersion, tradeSize);
+                   
+                    // set buy order version to the fully formed newBuyOrderVersion (with sql auto generated id) for the next performance of the while loop
+                    orderVersionRepository.save(newSellOrderVersion);
+
+                }
+
+            } else {
+                trading = false;
+            }
+        }
+           
+    }
+    
+    private OrderVersion copyOrderVersion(OrderVersion old, int tradeSize){
+        OrderVersion newOne = new OrderVersion();
+        newOne.setIsActive(true);
+        newOne.setOrder(old.getOrder());
+        newOne.setPrice(old.getPrice());
+        newOne.setTime(LocalDateTime.now());
+        newOne.setSize(old.getSize() - tradeSize);
+        return newOne;
+        
+    }
+    
     // ORDER VERSIONs
     @Override
     public List<OrderVersion> getActiveOrderVersionsForStock(Stock stock, boolean buy){
@@ -99,12 +182,14 @@ public class ServiceLayerImpl implements serviceLayer{
     
     @Override
     public List<Trade> getTradesForDay(LocalDate day){
-        return tradeRepository.getTradesBetweenTimes(day.atStartOfDay(), day.atTime(24, 0));
+        LocalDate dayPlusOne = day.plusDays(1);
+        return tradeRepository.getTradesBetweenTimes(day.atStartOfDay(), dayPlusOne.atStartOfDay());
     }
     
     @Override
     public List<Trade> getTradesForDayAndStock(LocalDate day, Stock stock){
-        return tradeRepository.getTradesBetweenTimesForStock(stock, day.atStartOfDay(), day.atTime(24, 0));
+        LocalDate dayPlusOne = day.plusDays(1);
+        return tradeRepository.getTradesBetweenTimesForStock(stock, day.atStartOfDay(), dayPlusOne.atStartOfDay());
     }
     
     @Override
